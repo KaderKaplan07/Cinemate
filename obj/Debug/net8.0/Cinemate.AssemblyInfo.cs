a9e00dc73a3394171c7c4e5f3e87d6c119d900d7
KaderKaplan07 <kaplankader695@gmail.com>
2025-12-25 22:11:58 +0300

    connection string duzeltildi

--- obj/Debug/net8.0/Cinemate.AssemblyInfo.cs
+++ obj/Debug/net8.0/Cinemate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinemate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c21825a639d285faf7fa8714091536cecb30d62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinemate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinemate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
